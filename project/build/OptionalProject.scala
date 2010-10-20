4b48f2f4dc5a8b115d5018c232ebe9dab30808fc
Lee Mighdoll <lee@underneath.ca>
2010-10-19 22:23:47 -0700

    cosmetic changes to test a commit with braid.

--- project/build/OptionalProject.scala
+++ project/build/OptionalProject.scala
@@ -5,4 +5,5 @@ class OptionalProject(info: ProjectInfo) extends DefaultProject(info) {
   val localIvy     = "Local Ivy" at "file://"+Path.userHome+"/.ivy2/local"
   
   val paranamer  = "com.thoughtworks.paranamer" % "paranamer" % "2.2.1"
+  override def managedStyle = ManagedStyle.Maven
 }
